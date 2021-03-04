37cff295f3ee35338aceae0553b8aa66f16f177c
Aulia <aulachmadgmail.com>
2021-03-04 11:58:38 +0700

    pre-release version

--- js/main.js
+++ js/main.js
@@ -1,117 +1,86 @@
 // const for DOM ------------------ //
 const btn = document.querySelector("button")
 const squares = document.querySelectorAll(".square")
-const sqOne = document.querySelector("#one")
-const sqTwo = document.querySelector("#two")
-const sqThree = document.querySelector("#three")
-const sqFour = document.querySelector("#four")
-const sqFive = document.querySelector("#five")
-const sqSix = document.querySelector("#six")
-const sqSeven = document.querySelector("#seven")
-const sqEight = document.querySelector("#eight")
-const sqNine = document.querySelector("#nine")
-// const for boxes (red)
-const b1 = getComputedStyle(sqOne).backgroundColor === "rgb(255, 0, 0)"
-const b2 = getComputedStyle(sqTwo).backgroundColor === "rgb(255, 0, 0)"
-const b3 = getComputedStyle(sqThree).backgroundColor === "rgb(255, 0, 0)"
-const b4 = getComputedStyle(sqFour).backgroundColor === "rgb(255, 0, 0)"
-const b5 = getComputedStyle(sqFive).backgroundColor === "rgb(255, 0, 0)"
-const b6 = getComputedStyle(sqSix).backgroundColor === "rgb(255, 0, 0)"
-const b7 = getComputedStyle(sqSeven).backgroundColor === "rgb(255, 0, 0)"
-const b8 = getComputedStyle(sqEight).backgroundColor === "rgb(255, 0, 0)"
-const b9 = getComputedStyle(sqNine).backgroundColor === "rgb(255, 0, 0)"
-// const for boxes (blue)
-const b1b = getComputedStyle(sqOne).backgroundColor === "rgb(0, 0, 255)"
-const b2b = getComputedStyle(sqTwo).backgroundColor === "rgb(0, 0, 255)"
-const b3b = getComputedStyle(sqThree).backgroundColor === "rgb(0, 0, 255)"
-const b4b = getComputedStyle(sqFour).backgroundColor === "rgb(0, 0, 255)"
-const b5b = getComputedStyle(sqFive).backgroundColor === "rgb(0, 0, 255)"
-const b6b = getComputedStyle(sqSix).backgroundColor === "rgb(0, 0, 255)"
-const b7b = getComputedStyle(sqSeven).backgroundColor === "rgb(0, 0, 255)"
-const b8b = getComputedStyle(sqEight).backgroundColor === "rgb(0, 0, 255)"
-const b9b = getComputedStyle(sqNine).backgroundColor === "rgb(0, 0, 255)"
-// const for boxes (white)
-const b1w = getComputedStyle(sqOne).backgroundColor === "rgb(255, 255, 255)"
-const b2w = getComputedStyle(sqTwo).backgroundColor === "rgb(255, 255, 255)"
-const b3w = getComputedStyle(sqThree).backgroundColor === "rgb(255, 255, 255)"
-const b4w = getComputedStyle(sqFour).backgroundColor === "rgb(255, 255, 255)"
-const b5w = getComputedStyle(sqFive).backgroundColor === "rgb(255, 255, 255)"
-const b6w = getComputedStyle(sqSix).backgroundColor === "rgb(255, 255, 255)"
-const b7w = getComputedStyle(sqSeven).backgroundColor === "rgb(255, 255, 255)"
-const b8w = getComputedStyle(sqEight).backgroundColor === "rgb(255, 255, 255)"
-const b9w = getComputedStyle(sqNine).backgroundColor === "rgb(255, 255, 255)"
+const overlay = document.querySelector("#overlay")
+const whoWon = document.querySelector("#whoWon")
+const playerTurn = document.querySelector("#playerTurn")
 // expr for logic ------------------ //
-squares.forEach(item => { // The purpose of this loop is to make every square listened
-  item.addEventListener("click", function(){
-    item.style.backgroundColor = "red"; // This line is for making the clicked square turns red
-    // conditionals for bot ----------- //
-    setTimeout(function(){
-      if (getComputedStyle(item).backgroundColor === "rgb(255, 0, 0)"){
-        // first row ------------------ //
-        if(Math.random() < 1/3){
-          if(Math.random() < 1/3 && (getComputedStyle(sqOne).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqOne).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya1")
-            sqOne.style.backgroundColor = "blue"
-          }else if(1/3 < Math.random() < 2/3 && (getComputedStyle(sqTwo).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqTwo).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya2")
-            sqTwo.style.backgroundColor = "blue"
-          }else if(getComputedStyle(sqThree).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqThree).backgroundColor !== "rgb(0, 0, 255)"){
-            // console.log("heya3")
-            sqThree.style.backgroundColor = "blue"
-          }
-        }
-        // second row ---------------- //
-        else if(1/3 < Math.random() < 2/3){
-          if(Math.random() < 1/3 && (getComputedStyle(sqFour).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqFour).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya4")
-            sqFour.style.backgroundColor = "blue"
-          }else if(1/3 < Math.random() < 2/3 && (getComputedStyle(sqFive).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqFive).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya5")
-            sqFive.style.backgroundColor = "blue"
-          }else if(getComputedStyle(sqSix).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqSix).backgroundColor !== "rgb(0, 0, 255)"){
-            // console.log("heya6")
-            sqSix.style.backgroundColor = "blue"
-          }
-        }
-        // third row ---------------- //
-        else{
-          if(Math.random() < 1/3 && (getComputedStyle(sqSeven).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqSeven).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya7")
-            sqSeven.style.backgroundColor = "blue"
-          }else if(1/3 < Math.random() < 2/3 && (getComputedStyle(sqEight).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqEight).backgroundColor !== "rgb(0, 0, 255)")){
-            // console.log("heya8")
-            sqEight.style.backgroundColor = "blue"
-          }else if(getComputedStyle(sqNine).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqNine).backgroundColor !== "rgb(0, 0, 255)"){
-            // console.log("heya9")
-            sqNine.style.backgroundColor = "blue"
-          }
-        }
+let game = {
+  currentPlayer : "X",
+  winCond : [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+  gameCond : ["", "", "", "", "", "", "", "", ""],
+  domPrint : function(){
+    playerTurn.innerText = `Player ${this.currentPlayer} turn`
+  },
+  playerHandler : function(){
+    this.currentPlayer === "X" ? this.currentPlayer = "O" : this.currentPlayer = "X";
+    playerTurn.innerText = `Player ${this.currentPlayer} turn`;
+  }
+}
+
+let boxClick = {
+  clickHandler : function(){
+    squares.forEach(item => {
+      item.addEventListener("click", function(){
+        item.innerText = game.currentPlayer;
+        game.gameCond[Number(item.getAttribute("data-cell-index"))] = game.currentPlayer;
+        resultCheck.run()
+      })
+    })
+  }
+}
+
+let resultCheck = {
+  run : function(){
+    let roundWon = false;
+    for (let i = 0; i < 8; i++){
+      const winCheck = game.winCond[i]
+      let c1 = game.gameCond[winCheck[0]]
+      let c2 = game.gameCond[winCheck[1]]
+      let c3 = game.gameCond[winCheck[2]]
+      if (c1 === "" || c2 === "" || c3 === ""){
+        continue;
       }
-    }, 300)
-  })
-  // expr for new Game ---------------- //
-  btn.addEventListener("click", function(){
-    item.removeAttribute("style")
-  })
+      if (c1 === c2 && c2 === c3){
+        roundWon = true;
+        break;
+      }
+    }
+    if (roundWon){
+      whoWon.innerText = `Player ${game.currentPlayer} win!`;
+      overlay.classList.add("overlay")
+      return;
+    }
+    if (!game.gameCond.includes("")){
+      whoWon.innerText = `It's a draw!`
+      overlay.classList.add("overlay")
+      return;
+    }
+    game.playerHandler();
+  }
+}
 
-})
-
-// if (getComputedStyle(sqOne).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqOne).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqOne.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqTwo).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqTwo).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqTwo.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqThree).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqThree).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqThree.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqFour).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqFour).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqFour.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqFive).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqFive).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqFive.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqSix).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqSix).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqSix.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqSeven).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqSeven).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqSeven.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqEight).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqEight).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqEight.style.backgroundColor = "blue"
-// }else if (getComputedStyle(sqNine).backgroundColor !== "rgb(255, 0, 0)" && getComputedStyle(sqNine).backgroundColor !== "rgb(0, 0, 255)"){
-//   sqNine.style.backgroundColor = "blue"
-// }
+let newGame = {
+  run : function(){
+    btn.addEventListener("click", function(){
+      game.gameCond = ["", "", "", "", "", "", "", "", ""];
+      game.currentPlayer = "X";
+      squares.forEach(item => item.innerText = null);
+      whoWon.innerText = "";
+      overlay.classList.remove("overlay");
+    })
+  }
+}
+// method call ------------------- //
+game.domPrint();
+boxClick.clickHandler();
+newGame.run();
